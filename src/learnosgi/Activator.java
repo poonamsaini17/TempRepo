1e56aa65627319c9943a7e7231a5ea8adb6368b9
poonamsaini17 <Poonam@Poonams-MacBook-Pro.local>
2018-03-06 00:53:31 +0530

    5th March Commit

---
+++ src/learnosgi/Activator.java
@@ -0,0 +1,24 @@
+package learnosgi;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		System.out.println("Hi I started with this OSGi Learning test!!");
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Sometimes you don't say Goodbye !!");
+	}
+
+}
